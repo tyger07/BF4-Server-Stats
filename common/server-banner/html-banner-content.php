5da9728a0cbdb3ac681e5b63fb81cb25ff4b6a3b
tyger07 <ty_ger07@hotmail.com>
2017-05-13 16:43:47 -0500

    Update html-banner-content.php

--- common/server-banner/html-banner-content.php
+++ common/server-banner/html-banner-content.php
@@ -284,8 +284,35 @@ $count = 0;
 // initialize timestamp values
 $now_timestamp = time();
 $old = $now_timestamp - 10800;
+// check to see if this top twenty cache table exists
+@mysqli_query($BF4stats,"
+	CREATE TABLE IF NOT EXISTS `tyger_stats_top_twenty_cache`
+	(
+		`ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		`PlayerID` INT(10) UNSIGNED NOT NULL,
+		`GID` TINYINT(4) UNSIGNED NOT NULL,
+		`SID` VARCHAR(100) NOT NULL,
+		`SoldierName` VARCHAR(45) NOT NULL,
+		`Score` INT(11) NOT NULL DEFAULT '0',
+		`Kills` INT(11) NOT NULL DEFAULT '0',
+		`KDR` VARCHAR(20) NOT NULL,
+		`HSR` VARCHAR(20) NOT NULL,
+		`timestamp` INT(11) NOT NULL DEFAULT '0',
+		PRIMARY KEY (`ID`),
+		UNIQUE `UNIQUE_TopTwentyData` (`PlayerID`, `GID`, `SID`),
+		INDEX `PlayerID` (`PlayerID` ASC),
+		INDEX `GID` (`GID` ASC),
+		INDEX `SID` (`SID` ASC),
+		INDEX `SoldierName` (`SoldierName` ASC),
+		INDEX `Score` (`Score` ASC),
+		INDEX `timestamp` (`timestamp` ASC),
+		CONSTRAINT `fk_tyger_stats_top_twenty_cache_PlayerID` FOREIGN KEY (`PlayerID`) REFERENCES `tbl_playerdata`(`PlayerID`) ON DELETE CASCADE ON UPDATE CASCADE,
+		CONSTRAINT `fk_tyger_stats_top_twenty_cache_GID` FOREIGN KEY (`GID`) REFERENCES `tbl_games`(`GameID`) ON DELETE CASCADE ON UPDATE CASCADE
+	)
+	ENGINE=InnoDB
+");
 // check if this is a top 20 player
-// if so, we can get their score rank much faster
+// if so, we can get their score rank much faster from the cache
 $Score_q = @mysqli_query($BF4stats,"
 	SELECT `Score`, `SoldierName`
 	FROM `tyger_stats_top_twenty_cache`
@@ -295,6 +322,7 @@ $Score_q = @mysqli_query($BF4stats,"
 	GROUP BY `PlayerID`
 	ORDER BY tps.`Score` DESC, tpd.`SoldierName` ASC LIMIT 10
 ");
+// if no recent results, do the slower query from live stats
 if(@mysqli_num_rows($Score_q) == 0)
 {
 	$Score_q = @mysqli_query($BF4stats,"
