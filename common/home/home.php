a7504e076f6d529fd4d433b4fc3eb13ce5348999
tyger07 <6035184+tyger07@users.noreply.github.com>
2022-10-01 08:22:26 -0700

    Update home.php
    forgot a parenthesis

--- common/home/home.php
+++ common/home/home.php
@@ -68,7 +68,7 @@ if(!empty($ServerID))
 		WHERE `ServerID` = {$ServerID}
 	");
 	$TotalRows_r = @mysqli_fetch_assoc($TotalRows_q);
-	if(!empty($TotalRows_r)
+	if(!empty($TotalRows_r))
 	{
 		$numrows = $TotalRows_r['CountPlayers'];
 	}
